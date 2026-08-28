535eca689e0de75f26f055260119cfa45a2fc144
Valhyi <jvgc97@outlook.cl>
2026-08-28 19:04:25 -0400

    Fix event bus annotation in ClientModEvents

--- src/main/java/com/valhyi/recyclertable/event/ClientModEvents.java
+++ src/main/java/com/valhyi/recyclertable/event/ClientModEvents.java
@@ -5,12 +5,10 @@ import com.valhyi.recyclertable.gui.RecyclerScreen;
 import com.valhyi.recyclertable.init.ModMenuTypes;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 
-// ES: Corregido el parámetro bus eliminando la referencia a la clase estática inexistente
-// EN: Corrected bus parameter removing reference to the non-existent static class
-@EventBusSubscriber(modid = RecyclerTable.MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = RecyclerTable.MOD_ID, value = Dist.CLIENT)
 public class ClientModEvents {
 
     @SubscribeEvent
