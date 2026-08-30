ac82f850b81e83e9c9cc990afd767d9b04d3468c
Valhyi <jvgc97@outlook.cl>
2026-08-30 19:57:42 -0400

    Refactor ModBlocks to use DeferredRegister generics

--- src/main/java/com/valhyi/recyclertable/init/ModBlocks.java
+++ src/main/java/com/valhyi/recyclertable/init/ModBlocks.java
@@ -2,28 +2,25 @@ package com.valhyi.recyclertable.init;
 
 import com.valhyi.recyclertable.RecyclerTable;
 import com.valhyi.recyclertable.block.RecyclerBlock;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
-import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(RecyclerTable.MOD_ID);
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(RecyclerTable.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, RecyclerTable.MOD_ID);
 
-    public static final DeferredBlock<RecyclerBlock> RECYCLER_TABLE = BLOCKS.registerBlock("recycler_table",
-        RecyclerBlock::new,
-        () -> BlockBehaviour.Properties.of()
-            .strength(3.5f, 6.0f)
-            .sound(SoundType.WOOD)
-            .requiresCorrectToolForDrops()
-    );
+    public static final DeferredBlock<Block> RECYCLER_TABLE = BLOCKS.register("recycler_table",
+            () -> new RecyclerBlock(BlockBehaviour.Properties.of().strength(2.5f)));
 
     public static final DeferredHolder<Item, Item> RECYCLER_TABLE_ITEM = ITEMS.register("recycler_table",
-    () -> new BlockItem(RECYCLER_TABLE.get(), new Item.Properties()));
+            () -> new BlockItem(RECYCLER_TABLE.get(), new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
