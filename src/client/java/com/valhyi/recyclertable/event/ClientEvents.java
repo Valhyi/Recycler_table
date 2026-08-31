58b825d83e35db671156cfdc744f5f70b51614e7
Valhyi <jvgc97@outlook.cl>
2026-08-31 16:16:19 -0400

    Update EventBusSubscriber annotation for ClientEvents

--- src/client/java/com/valhyi/recyclertable/event/ClientEvents.java
+++ src/client/java/com/valhyi/recyclertable/event/ClientEvents.java
@@ -8,7 +8,7 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 
-@EventBusSubscriber(modid = RecyclerTable.MOD_ID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = RecyclerTable.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
 public class ClientEvents {
     @SubscribeEvent
     public static void registerScreens(RegisterMenuScreensEvent event) {
