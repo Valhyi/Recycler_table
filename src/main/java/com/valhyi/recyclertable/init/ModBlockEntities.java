51be2bc52681e311c305fbab8b72d970645c9584
Valhyi <jvgc97@outlook.cl>
2026-08-30 20:07:41 -0400

    Refactor recycler_be registration to use Set

--- src/main/java/com/valhyi/recyclertable/init/ModBlockEntities.java
+++ src/main/java/com/valhyi/recyclertable/init/ModBlockEntities.java
@@ -8,13 +8,15 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.Set;
+
 public class ModBlockEntities {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES =
             DeferredRegister.create(Registries.BLOCK_ENTITY_TYPE, RecyclerTable.MOD_ID);
 
     public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<RecyclerBlockEntity>> RECYCLER_BE =
             BLOCK_ENTITIES.register("recycler_be",
-                    () -> BlockEntityType.Builder.of(RecyclerBlockEntity::new, ModBlocks.RECYCLER_TABLE.get()).build(null));
+                    () -> new BlockEntityType<>(RecyclerBlockEntity::new, Set.of(ModBlocks.RECYCLER_TABLE.get())));
 
     public static void register(IEventBus eventBus) {
         BLOCK_ENTITIES.register(eventBus);
