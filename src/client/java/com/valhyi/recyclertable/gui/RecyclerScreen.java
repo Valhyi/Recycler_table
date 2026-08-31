a4c785cb1f8a60c83ed7ef62fb0247f101c61647
Valhyi <jvgc97@outlook.cl>
2026-08-31 15:42:13 -0400

    Refactor RecyclerScreen for improved rendering

--- src/client/java/com/valhyi/recyclertable/gui/RecyclerScreen.java
+++ src/client/java/com/valhyi/recyclertable/gui/RecyclerScreen.java
@@ -3,28 +3,30 @@ package com.valhyi.recyclertable.gui;
 import com.valhyi.recyclertable.RecyclerTable;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
 public class RecyclerScreen extends AbstractContainerScreen<RecyclerMenu> {
-    private static final ResourceLocation TEXTURE = 
-        ResourceLocation.fromNamespaceAndPath(RecyclerTable.MOD_ID, "textures/gui/recycler_gui.png");
 
-    public RecyclerScreen(RecyclerMenu menu, Inventory playerInventory, Component title) {
-        super(menu, playerInventory, title);
+    private static final ResourceLocation TEXTURE = 
+            ResourceLocation.fromNamespaceAndPath(RecyclerTable.MOD_ID, "textures/gui/recycler_gui.png");
+
+    public RecyclerScreen(RecyclerMenu menu, Inventory inventory, Component title) {
+        super(menu, inventory, title);
     }
 
     @Override
     protected void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
         int x = (this.width - this.imageWidth) / 2;
         int y = (this.height - this.imageHeight) / 2;
-        guiGraphics.blit(TEXTURE, x, y, 0, 0, this.imageWidth, this.imageHeight);
+        guiGraphics.blit(RenderType::guiTextured, TEXTURE, x, y, 0, 0, this.imageWidth, this.imageHeight, 256, 256);
     }
 
     @Override
-    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        super.render(guiGraphics, mouseX, mouseY, partialTick);
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float delta) {
+        super.render(guiGraphics, mouseX, mouseY, delta);
         this.renderTooltip(guiGraphics, mouseX, mouseY);
     }
 }
