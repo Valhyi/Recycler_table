a63420e5773673dd49bdea770c1841c6f26d8214
Valhyi <jvgc97@outlook.cl>
2026-08-31 16:10:17 -0400

    Add event subscription for menu screen registration

--- src/client/java/com/valhyi/recyclertable/event/ClientEvents.java
+++ src/client/java/com/valhyi/recyclertable/event/ClientEvents.java
@@ -1,10 +1,16 @@
 package com.valhyi.recyclertable.event;
 
+import com.valhyi.recyclertable.RecyclerTable;
 import com.valhyi.recyclertable.gui.RecyclerScreen;
 import com.valhyi.recyclertable.init.ModMenuTypes;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 
+@EventBusSubscriber(modid = RecyclerTable.MOD_ID, value = Dist.CLIENT)
 public class ClientEvents {
+    @SubscribeEvent
     public static void registerScreens(RegisterMenuScreensEvent event) {
         event.register(ModMenuTypes.RECYCLER_MENU.get(), RecyclerScreen::new);
     }
