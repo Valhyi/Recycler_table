5fecab6440993c54fe10948ee0f61c16c129f065
Valhyi <jvgc97@outlook.cl>
2026-09-01 19:12:24 -0400

    Fix: Agregar renderizado de fondo de GUI con GuiGraphics y ResourceLocation para NeoForge 26.2.0.70

--- src/main/java/com/valhyi/recyclertable/gui/RecyclerScreen.java
+++ src/main/java/com/valhyi/recyclertable/gui/RecyclerScreen.java
@@ -1,14 +1,31 @@
 package com.valhyi.recyclertable.gui;
 
 import com.valhyi.recyclertable.RecyclerTable;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.InventoryMenu;
 
 public class RecyclerScreen extends AbstractContainerScreen<RecyclerMenu> {
+    private static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(RecyclerTable.MOD_ID, "textures/gui/recycler_gui.png");
 
     public RecyclerScreen(RecyclerMenu menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
+        this.imageWidth = 220;
+        this.imageHeight = 210;
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        // Posiciona la GUI centrada
+        this.leftPos = (this.width - this.imageWidth) / 2;
+        this.topPos = (this.height - this.imageHeight) / 2;
+    }
+
+    @Override
+    public void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
+        guiGraphics.blit(TEXTURE, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight, this.imageWidth, this.imageHeight);
     }
 }
