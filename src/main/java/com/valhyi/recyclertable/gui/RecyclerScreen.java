39ae4ce5a0ad28a53ce18f3dab1bbba8538f51d6
Valhyi <jvgc97@outlook.cl>
2026-09-01 19:17:02 -0400

    Fix: Remover asignación de campos finales imageWidth e imageHeight

--- src/main/java/com/valhyi/recyclertable/gui/RecyclerScreen.java
+++ src/main/java/com/valhyi/recyclertable/gui/RecyclerScreen.java
@@ -8,15 +8,5 @@ public class RecyclerScreen extends AbstractContainerScreen<RecyclerMenu> {
 
     public RecyclerScreen(RecyclerMenu menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
-        this.imageWidth = 220;
-        this.imageHeight = 210;
-    }
-
-    @Override
-    protected void init() {
-        super.init();
-        // Centra la GUI
-        this.leftPos = (this.width - this.imageWidth) / 2;
-        this.topPos = (this.height - this.imageHeight) / 2;
     }
 }
