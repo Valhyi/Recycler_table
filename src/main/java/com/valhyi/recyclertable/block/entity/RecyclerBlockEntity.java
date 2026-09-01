8d110ab2b2fb9e34637d386d4e06d04bf7beb0c6
Valhyi <jvgc97@outlook.cl>
2026-08-31 21:01:58 -0400

    Refactor RecyclerBlockEntity to use SimpleContainer

--- src/main/java/com/valhyi/recyclertable/block/entity/RecyclerBlockEntity.java
+++ src/main/java/com/valhyi/recyclertable/block/entity/RecyclerBlockEntity.java
@@ -1,19 +1,31 @@
 package com.valhyi.recyclertable.block.entity;
 
-import com.valhyi.recyclertable.block.RecyclerBlock;
 import com.valhyi.recyclertable.gui.RecyclerMenu;
 import com.valhyi.recyclertable.init.ModBlockEntities;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.NonNullList;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.ContainerHelper;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
 import org.jetbrains.annotations.Nullable;
 
 public class RecyclerBlockEntity extends BlockEntity implements MenuProvider {
+    private final SimpleContainer container = new SimpleContainer(21) {
+        @Override
+        public void setChanged() {
+            super.setChanged();
+            RecyclerBlockEntity.this.setChanged();
+        }
+    };
 
     public RecyclerBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.RECYCLER_BLOCK_ENTITY.get(), pos, state);
@@ -27,6 +39,30 @@ public class RecyclerBlockEntity extends BlockEntity implements MenuProvider {
     @Nullable
     @Override
     public AbstractContainerMenu createMenu(int containerId, Inventory playerInventory, Player player) {
-        return new RecyclerMenu(containerId, playerInventory, this.worldPosition);
+        return new RecyclerMenu(containerId, playerInventory, this.container);
+    }
+
+    public SimpleContainer getContainer() {
+        return container;
+    }
+
+    @Override
+    protected void saveAdditional(ValueOutput output) {
+        super.saveAdditional(output);
+        NonNullList<ItemStack> items = NonNullList.withSize(this.container.getContainerSize(), ItemStack.EMPTY);
+        for (int i = 0; i < this.container.getContainerSize(); i++) {
+            items.set(i, this.container.getItem(i));
+        }
+        ContainerHelper.saveAllItems(output, items);
+    }
+
+    @Override
+    protected void loadAdditional(ValueInput input) {
+        super.loadAdditional(input);
+        NonNullList<ItemStack> items = NonNullList.withSize(this.container.getContainerSize(), ItemStack.EMPTY);
+        ContainerHelper.loadAllItems(input, items);
+        for (int i = 0; i < items.size(); i++) {
+            this.container.setItem(i, items.get(i));
+        }
     }
 }
