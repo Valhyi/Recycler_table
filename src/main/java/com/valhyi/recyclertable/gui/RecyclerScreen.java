ec35d715ff582accdfd544d534409dde5514b0e7
Valhyi <jvgc97@outlook.cl>
2026-09-01 19:02:28 -0400

    Fix: Simplificar RecyclerScreen - remover imports y métodos no disponibles en NeoForge 26.2.0.70

--- src/main/java/com/valhyi/recyclertable/gui/RecyclerScreen.java
+++ src/main/java/com/valhyi/recyclertable/gui/RecyclerScreen.java
@@ -1,37 +1,14 @@
 package com.valhyi.recyclertable.gui;
 
 import com.valhyi.recyclertable.RecyclerTable;
-import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.InventoryMenu;
 
 public class RecyclerScreen extends AbstractContainerScreen<RecyclerMenu> {
-    private static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(RecyclerTable.MOD_ID, "textures/gui/recycler_gui.png");
 
     public RecyclerScreen(RecyclerMenu menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
-    }
-
-    @Override
-    protected void init() {
-        super.init();
-        // Establece el tamaño de la textura después de inicializar
-        this.imageWidth = 220;
-        this.imageHeight = 210;
-    }
-
-    @Override
-    protected void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
-        int x = (this.width - this.imageWidth) / 2;
-        int y = (this.height - this.imageHeight) / 2;
-        guiGraphics.blit(TEXTURE, x, y, 0, 0, this.imageWidth, this.imageHeight);
-    }
-
-    @Override
-    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        super.render(guiGraphics, mouseX, mouseY, partialTick);
-        this.renderTooltip(guiGraphics, mouseX, mouseY);
     }
 }
