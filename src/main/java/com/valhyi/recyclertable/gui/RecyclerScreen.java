04cd009fc10fb95bb6e26506b9cf31bb1daf9a3f
Valhyi <jvgc97@outlook.cl>
2026-08-31 20:26:06 -0400

    Refactor render methods in RecyclerScreen

--- src/main/java/com/valhyi/recyclertable/gui/RecyclerScreen.java
+++ src/main/java/com/valhyi/recyclertable/gui/RecyclerScreen.java
@@ -1,10 +1,8 @@
 package com.valhyi.recyclertable.gui;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.valhyi.recyclertable.RecyclerTable;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
@@ -18,15 +16,13 @@ public class RecyclerScreen extends AbstractContainerScreen<RecyclerMenu> {
 
     @Override
     protected void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         int x = (this.width - this.imageWidth) / 2;
         int y = (this.height - this.imageHeight) / 2;
-        guiGraphics.blit(RenderType::guiTextured, TEXTURE, x, y, 0, 0, this.imageWidth, this.imageHeight, 256, 256);
+        guiGraphics.blit(TEXTURE, x, y, 0, 0, this.imageWidth, this.imageHeight);
     }
 
     @Override
     public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        this.renderBackground(guiGraphics, mouseX, mouseY, partialTick);
         super.render(guiGraphics, mouseX, mouseY, partialTick);
         this.renderTooltip(guiGraphics, mouseX, mouseY);
     }
