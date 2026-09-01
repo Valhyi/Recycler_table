74d95388dab521953183b90758d92d865703ecd8
Valhyi <jvgc97@outlook.cl>
2026-09-01 19:21:56 -0400

    Add: Renderizado de GUI con GuiGraphics para mostrar textura de fondo

--- src/main/java/com/valhyi/recyclertable/gui/RecyclerScreen.java
+++ src/main/java/com/valhyi/recyclertable/gui/RecyclerScreen.java
@@ -1,12 +1,33 @@
 package com.valhyi.recyclertable.gui;
 
+import com.valhyi.recyclertable.RecyclerTable;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.InventoryMenu;
 
 public class RecyclerScreen extends AbstractContainerScreen<RecyclerMenu> {
+    private static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(RecyclerTable.MOD_ID, "textures/gui/recycler_gui.png");
+    private static final int TEXTURE_WIDTH = 220;
+    private static final int TEXTURE_HEIGHT = 210;
 
     public RecyclerScreen(RecyclerMenu menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
+        this.imageWidth = TEXTURE_WIDTH;
+        this.imageHeight = TEXTURE_HEIGHT;
+    }
+
+    @Override
+    public void renderBg(GuiGraphics guiGraphics, float partialTick, int mouseX, int mouseY) {
+        RenderSystem.setShader(GameRenderer::getPositionColorShader);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        int x = (this.width - this.imageWidth) / 2;
+        int y = (this.height - this.imageHeight) / 2;
+        
+        this.minecraft.getTextureManager().bindForSetup(TEXTURE);
+        guiGraphics.blit(x, y, 0, 0, this.imageWidth, this.imageHeight, this.imageWidth, this.imageHeight);
     }
 }
