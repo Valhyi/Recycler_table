ec17ba80d509ef46410466d54d72d58d847b666a
Valhyi <jvgc97@outlook.cl>
2026-09-02 11:21:31 -0400

    Implement: RecyclerBlockEntity con lógica de procesamiento y ticks

--- src/main/java/com/valhyi/recyclertable/block/entity/RecyclerBlockEntity.java
+++ src/main/java/com/valhyi/recyclertable/block/entity/RecyclerBlockEntity.java
@@ -13,7 +13,6 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.ValueInput;
